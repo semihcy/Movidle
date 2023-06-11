0f3af2d9f59c71ed1d8a03359c653266f65b5f80
SemihCay <52semih42@gmail.com>
2023-06-11 23:49:04 +0300

    initial

--- src/main/java/com/cuhacay/movidle/HelloApplication.java
+++ src/main/java/com/cuhacay/movidle/HelloApplication.java
@@ -10,11 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        System.out.println("Hello this will be  the best project ever.");
     }
 
     public static void main(String[] args) {
