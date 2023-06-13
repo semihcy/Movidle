4c37eab5576cef45bbec80b85fca66c4ce963c53
SemihCay <52semih42@gmail.com>
2023-06-13 23:40:50 +0300

    CSVReader, look it

--- src/main/java/com/cuhacay/movidle/HelloApplication.java
+++ src/main/java/com/cuhacay/movidle/HelloApplication.java
@@ -6,18 +6,39 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class HelloApplication extends Application {
+public  class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        URL url = getClass().getClassLoader().getResource("imdb_top_250_with_images.csv");
+        File file = new File(url.getFile());
+
+        ArrayList<String[]> films = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(",");
+                films.add(row);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Parent parent = fxmlLoader.load();
         parent.setStyle("-fx-background-color: gray");
 
         AutoCompleteTextField textField = (AutoCompleteTextField) fxmlLoader.getNamespace().get("autoComplete");
-        textField.setResource(Arrays.asList("Ahmet", "İsa", "Mehmet", "Ali Kemal", "Yunus", "Tunahan"));
+        textField.setResource(Arrays.asList(String.valueOf(films)));
 
         Scene scene = new Scene(parent, 500, 500);
         stage.setTitle("Fuck you!!");
@@ -25,7 +46,5 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    public static void main(String[] args) { launch(); }
 }
