088e3c6e782f08d8981db759901dc69286537aa6
Semih Çay <52semih42@gmail.com>
2023-06-16 18:17:32 +0300

    Lets MVC

--- src/main/java/com/cuhacay/movidle/HelloApplication.java
+++ src/main/java/com/cuhacay/movidle/HelloApplication.java
@@ -1,19 +1,12 @@
 package com.cuhacay.movidle;
 
-import com.cuhacay.movidle.autocomplete.VisualAutoCompleteTextField;
-import com.cuhacay.movidle.autocomplete.model.Suggestion;
-import com.cuhacay.movidle.autocomplete.model.VisualSuggestion;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+
 
 
 public class HelloApplication extends Application {
--- src/main/java/com/cuhacay/movidle/HelloController.java
+++ src/main/java/com/cuhacay/movidle/HelloController.java
@@ -4,6 +4,7 @@ import com.cuhacay.movidle.autocomplete.VisualAutoCompleteTextField;
 import com.cuhacay.movidle.autocomplete.model.Suggestion;
 import com.cuhacay.movidle.autocomplete.model.VisualSuggestion;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,10 +14,11 @@ import javafx.scene.layout.VBox;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Constructor;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
 
     @FXML
     public HBox labels;
@@ -33,7 +35,30 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
-    public HelloController(){
+    @FXML
+    protected void onClickSubmit(){
+
+        String userInput = autoComplete.getText();
+        boolean userInputExists = movies.stream()
+                .map(Movie::getName)
+                .anyMatch(name -> name.equals(userInput));
+        if (userInputExists) {
+            System.out.println(userInput);
+        } else {
+            showErrorDialog("Invalid input!", "Please enter valid input.");
+        }
+
+    }
+    private void showErrorDialog(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         try (BufferedReader br = new BufferedReader(new FileReader("imdb_top_250_with_images.csv"))) {
             String line="";
             boolean isFirstLine = true;
@@ -70,27 +95,5 @@ public class HelloController {
         RandomMovie movieSelector = new RandomMovie(movies);
         randomMovie = movieSelector.selectRandomMovie();
         System.out.println("Random Film: " + randomMovie.getName());
-    }
-
-    @FXML
-    protected void onClickSubmit(){
-
-        String userInput = autoComplete.getText();
-        boolean userInputExists = movies.stream()
-                .map(Movie::getName)
-                .anyMatch(name -> name.equals(userInput));
-        if (userInputExists) {
-            System.out.println(userInput);
-        } else {
-            showErrorDialog("Invalid input!", "Please enter valid input.");
-        }
-
-    }
-    private void showErrorDialog(String title, String message) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
     }
 }
