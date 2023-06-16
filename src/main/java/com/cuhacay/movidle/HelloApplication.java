0d2d5d4468d0143a0029018dd56450c634c437f9
Semih Çay <52semih42@gmail.com>
2023-06-16 18:08:32 +0300

    Errors

--- src/main/java/com/cuhacay/movidle/HelloApplication.java
+++ src/main/java/com/cuhacay/movidle/HelloApplication.java
@@ -19,67 +19,11 @@ import java.util.ArrayList;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        ArrayList<Movie> movies = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("imdb_top_250_with_images.csv"))) {
-            String line="";
-            boolean isFirstLine = true;
-            while ((line = br.readLine()) != null) {
-                if (isFirstLine){
-                    isFirstLine = false;
-                    continue;
-                }
-                String[] row = line.split(";");
-                int year;
-                try{
-                    year = Integer.parseInt(row[2]);
-                }catch (NumberFormatException e){
-                    System.out.println("Invalid year format:" + row[2]);
-                    continue;
-                }
-                Movie movie = new Movie(
-                        row[1],
-                        year,
-                        row[3],
-                        row[4],
-                        row[5],
-                        row[6],
-                        row[8]
-                );
-                movies.add(movie);
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        RandomMovie movieSelector = new RandomMovie(movies);
-        Movie randomMovie = movieSelector.selectRandomMovie();
-        System.out.println("Random Film: " + randomMovie.getName());
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Parent parent = fxmlLoader.load();
         parent.setStyle("-fx-background-color: gray");
-
-        VisualAutoCompleteTextField textField = (VisualAutoCompleteTextField) fxmlLoader.getNamespace().get("autoComplete");
-        textField.setResource(movies.stream().map(movie -> (Suggestion) new VisualSuggestion(movie.getName(),movie.getImageUrl())).toList());
-
-
-
-        Button submitButton = (Button) fxmlLoader.getNamespace().get("submitButton");
-        submitButton.setOnAction(event -> {
-            String userInput = textField.getText();
-            boolean userInputExists = movies.stream()
-                    .map(Movie::getName)
-                    .anyMatch(name -> name.equals(userInput));
-            if (userInputExists) {
-                System.out.println(userInput);
-            } else {
-                showErrorDialog("Invalid input!", "Please enter valid input.");
-            }
-        });
-
-
 
         Scene scene = new Scene(parent, 500, 500);
         stage.setTitle("Hello Application");
@@ -88,11 +32,4 @@ public class HelloApplication extends Application {
     }
     public static void main(String[] args) { launch();}
 
-    private void showErrorDialog(String title, String message) {
-        Alert alert = new Alert(Alert.AlertType.ERROR);
-        alert.setTitle(title);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        alert.showAndWait();
-    }
 }
--- src/main/java/com/cuhacay/movidle/HelloController.java
+++ src/main/java/com/cuhacay/movidle/HelloController.java
@@ -1,15 +1,96 @@
 package com.cuhacay.movidle;
 
+import com.cuhacay.movidle.autocomplete.VisualAutoCompleteTextField;
+import com.cuhacay.movidle.autocomplete.model.Suggestion;
+import com.cuhacay.movidle.autocomplete.model.VisualSuggestion;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 
 public class HelloController {
+
+    @FXML
+    public HBox labels;
+    @FXML
+    public Button submitButton;
+    @FXML
+    public VisualAutoCompleteTextField autoComplete;
+    @FXML
+    public VBox root;
+
+    private Movie randomMovie;
+
+    ArrayList<Movie> movies = new ArrayList<>();
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("This is Movidle");
+    public HelloController(){
+        try (BufferedReader br = new BufferedReader(new FileReader("imdb_top_250_with_images.csv"))) {
+            String line="";
+            boolean isFirstLine = true;
+            while ((line = br.readLine()) != null) {
+                if (isFirstLine){
+                    isFirstLine = false;
+                    continue;
+                }
+                String[] row = line.split(";");
+                int year;
+                try{
+                    year = Integer.parseInt(row[2]);
+                }catch (NumberFormatException e){
+                    System.out.println("Invalid year format:" + row[2]);
+                    continue;
+                }
+                Movie movie = new Movie(
+                        row[1],
+                        year,
+                        row[3],
+                        row[4],
+                        row[5],
+                        row[6],
+                        row[8]
+                );
+                movies.add(movie);
+            }
+            autoComplete.setResource(movies.stream().map(movie -> (Suggestion) new VisualSuggestion(movie.getName(),movie.getImageUrl())).toList());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        RandomMovie movieSelector = new RandomMovie(movies);
+        randomMovie = movieSelector.selectRandomMovie();
+        System.out.println("Random Film: " + randomMovie.getName());
+    }
+
+    @FXML
+    protected void onClickSubmit(){
+
+        String userInput = autoComplete.getText();
+        boolean userInputExists = movies.stream()
+                .map(Movie::getName)
+                .anyMatch(name -> name.equals(userInput));
+        if (userInputExists) {
+            System.out.println(userInput);
+        } else {
+            showErrorDialog("Invalid input!", "Please enter valid input.");
+        }
+
+    }
+    private void showErrorDialog(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 }
